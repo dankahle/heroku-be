299d43bccb4f4e1b41b3d0ba1f4f2e2ec773825e
Dan Kahle <dan.kahle@gmail.com>
2015-07-05 20:19:28 -0700

    added heroku2-be

--- projects/heroku1-be/userDb.js
+++ projects/heroku1-be/userDb.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV == 'production')
 else
 	connstr = 'mongodb://localhost:27017/db';
 
-console.log('mongo connected to:', process.env.NODE_ENV == 'production'? 'mongolab': 'localhost');
+console.log('heroku1be: mongo connected to:', process.env.NODE_ENV == 'production'? 'mongolab': 'localhost');
 var db = mongoose.createConnection(connstr)
 db.on('error', function (err) {
 	console.error(err);
